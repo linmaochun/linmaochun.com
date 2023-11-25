f39c6899177ef205a3668dba1ed14e7a4f576a4d
linmaochun <hi@linmaochun.com>
2023-11-26 03:05:55 +0800

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   .vscode/launch.json
    	new file:   README.md
    	new file:   astro.config.mjs
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/favicon.ico
    	new file:   public/placeholder-social.jpg
    	new file:   src/components/BaseHead.astro
    	new file:   src/config.ts
    	new file:   src/env.d.ts
    	new file:   src/layouts/BlogPost.astro
    	new file:   src/pages/about.md
    	new file:   src/pages/blog/first-post.md
    	new file:   src/pages/index.astro
    	new file:   src/pages/rss.xml.js
    	new file:   src/styles/global.css
    	new file:   tsconfig.json

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '林茂春';
+export const SITE_DESCRIPTION = "林茂春的个人网站 linmaochun's Personal Website";
